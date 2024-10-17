211d99350826fca1253bbd19aeedfe122f870232
Afif <49422146+Avwaveaf@users.noreply.github.com>
2024-10-17 11:17:26 +0700

    update: finishing compose story screen

--- app/src/main/java/com/avwaveaf/storyspace/view/home/ui/compose/ComposeStoryFragment.kt
+++ app/src/main/java/com/avwaveaf/storyspace/view/home/ui/compose/ComposeStoryFragment.kt
@@ -1,9 +1,15 @@
 package com.avwaveaf.storyspace.view.home.ui.compose
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.avwaveaf.storyspace.databinding.FragmentComposeStoryBinding
 
@@ -13,16 +19,48 @@ class ComposeStoryFragment : Fragment() {
     private var _binding: FragmentComposeStoryBinding? = null
     private val binding get() = _binding!!
 
+    private var currentUri: Uri? = null
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            // insert the current uri
+            currentUri = uri
+            // after we get the uri we can call showImage to inflate imageview with selected image
+            showImage()
+        } else {
+            Log.d("main_activity/photo_picker", "No Media Selected!")
+            Toast.makeText(requireContext(), "No Media Selected", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun showImage() {
+        currentUri?.let {
+            binding.ivImagePreview.scaleType = ImageView.ScaleType.CENTER_CROP
+            binding.ivImagePreview.setImageURI(it)
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentComposeStoryBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        return root
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnGallery.setOnClickListener {
+            startGallery()
+        }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
 
 
     override fun onDestroyView() {
